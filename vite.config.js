7b5d13be5df533363969c9f4f8831c9b911ceefe
SouliyaPPS-7575 <souliyapps@gmail.com>
2025-09-05 10:18:31 +0700

    chore: update app icons, manifest and sitemap timestamps

--- vite.config.js
+++ vite.config.js
@@ -41,28 +41,16 @@ export default defineConfig(({ mode }) => {
           start_url: '/',
           icons: [
             {
-              src: '/icons/Icon-192.png',
+              src: '/buddhaword.png',
               sizes: '192x192',
               type: 'image/png',
               purpose: 'any',
             },
             {
-              src: '/icons/Icon-512.png',
+              src: '/buddhaword.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any',
-            },
-            {
-              src: '/icons/Icon-maskable-192.png',
-              sizes: '192x192',
-              type: 'image/png',
-              purpose: 'maskable',
-            },
-            {
-              src: '/icons/Icon-maskable-512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'maskable',
             },
           ],
         },
